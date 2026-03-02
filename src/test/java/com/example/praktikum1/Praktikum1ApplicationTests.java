5e5dae4be0ffd2d160d33aad9a096dcfea4b6e21
jaddu17 <shafajaddu@gmail.com>
2026-03-02 15:41:18 +0700

    menambahkan validation

--- src/test/java/com/example/praktikum1/Praktikum1ApplicationTests.java
+++ src/test/java/com/example/praktikum1/Praktikum1ApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.praktikum1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Praktikum1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	public static void main(String[] args){
+        SpringApplication.run(Praktikum1ApplicationTests.class,args);
+    }
 
 }
